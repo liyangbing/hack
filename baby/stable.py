d240585fa0cc2cb8b8c95e70252ded1e26033a56
lybing315 <lybing315@163.com>
2023-07-30 19:01:56 +0800

    update

--- baby/stable.py
+++ baby/stable.py
@@ -3,14 +3,18 @@ from diffusers import StableDiffusionPipeline, DPMSolverMultistepScheduler
 
 model_id = "/data/stable-diffusion-2-1"
 
+system_prompt = ',for chidren'
+
 class PicGenerator:
     def __init__(self):
         self.pipe = StableDiffusionPipeline.from_pretrained(
             model_id, torch_dtype=torch.float16
         )
-        self.pipe.scheduler = DPMSolverMultistepScheduler.from_config(self.pipe.scheduler.config)
+        self.pipe.scheduler = DPMSolverMultistepScheduler.from_config(
+            self.pipe.scheduler.config)
         self.pipe = self.pipe.to("cuda")
 
     def generate(self, prompt):
-        image = self.pipe(prompt).images[0]
+        image = self.pipe(prompt + system_prompt, height=512, width=768).images[0]
         return image
+
