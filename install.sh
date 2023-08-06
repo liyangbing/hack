31fb7a91a68f0d93b1a561b5bd77221c34538a61
likefallwind <likefallwind@163.com>
2023-08-06 14:09:50 +0800

    add some change of directory

--- install.sh
+++ install.sh
@@ -1,15 +1,7 @@
-# install mysql
-apt-get update
-#apt-get install -y mysql-server
-#service mysql start
-
-# install python 
 pip install -r requirements.txt
 python -m spacy download zh_core_web_sm
-
-
 apt-get install libssl-dev
-export PYTHONPATH=.:$PYTHONPATH
-python database/data_init.py
-export OPENAI_API_KEY=
-export DID_API_KEY=
+export PYTHONPATH=$(pwd):$PYTHONPATH
+python ./database/data_init.py
+#export OPENAI_API_KEY=
+#export DID_API_KEY=
